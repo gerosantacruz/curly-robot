3b7842d82c76bcd45cd080f73988463f1869a9c9
gerosantacruz <geronimo.t.santacruz@gmail.com>
2022-06-01 09:48:41 -0300

    Joke api in Rust

---
+++ api/apirst/src/main.rs
@@ -0,0 +1,9 @@
+use reqwest;
+
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::blocking::get("https://v2.jokeapi.dev/joke/Programming,Dark?lang=es")?.text()?;
+    println!("{:#?}", resp);
+    Ok(())
+}
